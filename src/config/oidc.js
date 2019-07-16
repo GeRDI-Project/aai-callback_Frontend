69b04b99dbb7498e5a697448010b3e86d75c69e7
Nelson Tavares de Sousa <ntd@informatik.uni-kiel.de>
2019-07-16 12:49:37 +0200

    SAI-1720 reverted to keycloak

--- src/config/oidc.js
+++ src/config/oidc.js
@@ -1,10 +1,10 @@
 export const oidcSettings = {
-  authority: 'https://unity.zih.tu-dresden.de:2443/oauth2',
-  client_id: 'oauth-client',
+  authority: 'https://www.test.gerdi.org/admin/auth/realms/master',
+  client_id: 'test',
   redirect_uri: 'https://www.test.gerdi.org/callback/oidc-callback',
-  response_type: 'id_token',
+  response_type: 'id_token token',
   scope: 'openid profile',
   automaticSilentRenew: true,
   silent_redirect_uri: 'https://www.test.gerdi.org/callback/silent-renew-oidc.html',
-  post_logout_redirect_uri: 'https://www.test.gerdi.org/'
+  post_logout_redirect_uri: 'https://www.test.gerdi.org'
 }
